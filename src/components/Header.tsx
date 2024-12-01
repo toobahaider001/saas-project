f7433a2c973d2c91ab76e675274741cb4df08e5f
toobahaider001 <159890263+toobahaider001@users.noreply.github.com>
2024-12-01 12:10:24 +0500

    addition

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,71 @@
-import React from 'react'
-import Logo from './Logo'
-import Button from './Button'
+'use client'
+import React, { useState } from 'react'
 import Link from 'next/link'
+import { IoMdMenu } from 'react-icons/io';
+import Logo from './Logo';
+import Button from './Button';
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div className='flex justify-between items-center py-5 border-b-[0.5px] border-zinc-400/15'>
+    <div className='w-full flex justify-between items-center py-4 px-6'>
       <div>
         <Logo />
       </div>
-      <div className=' hidden md:flex gap-4 text-zinc-300 font-light text-sm '>
-      <Link href={"/"}>Home</Link>
-      <Link href={"/features"}>features</Link>
-      <Link href={"/contact"}>Contact</Link>
+
+      <nav className='hidden md:flex gap-4  space-x-6'>
+        <Link href="/" className="text-lg text-white">
+          Home
+        </Link>
+        <Link href="/about" className="text-lg text-white">
+          About
+        </Link>
+        <Link href="/contact" className="text-lg text-white">
+          Contact
+        </Link>
+        <Button text='Hire Now' />
+      </nav>
+
+      {/* Hamburger Menu Button for Small Screens */}
+      <div className='md:hidden'>
+        <button
+          className="p-2 focus:outline-none"
+          onClick={toggleMenu}
+          aria-label="Toggle menu"
+        >
+          <div className="space-y-1">
+            <span
+              className={`block transform transition-transform
+              `}
+            ><IoMdMenu  size={30} color='white'/></span>
+          </div>
+        </button>
       </div>
-      <div>
-        <Button text='Get a demo' />
+
+      {/* Menu for Small Screens */}
+      <div
+        className={`absolute top-14 right-4 bg-shadeblue shadow-md mt- rounded-md h-[1000px] w-[300px] p-4 space-y-2 transition-all duration-1000 md:hidden ${
+          isMenuOpen ? 'block' : 'hidden'
+        }`}
+      >
+        <Link href="/" className="block px-4 py-2 text-gray-700 hover:bg-gray-200">
+          Home
+        </Link>
+        <Link href="/about" className="block px-4 py-2 text-gray-700 hover:bg-gray-200">
+          About
+        </Link>
+        <Link href="/contact" className="block px-4 py-2 text-gray-700 hover:bg-gray-200">
+          Contact
+        </Link>
+        <Button text="Hire now" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
